c3d8a8b7f69aef16f1ca102b75dc97acacb13929
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-09-27 13:54:08 +0200

    modif02

--- jour01.js
+++ jour01.js
@@ -5,32 +5,33 @@ console.log("Hello World !");
 // 02
 
 var test = "My name is Syrine"
-
 console.log(test);
 
 // 03
 
 var name = " Syrine"
-
 console.log("Nice to meet you" + name)
 
 // 04
 
 var testLenght = "I'm very long !"
-console.log(`${testLenght.length}`)
+console.log(testLenght.length)
 
 // 05
 
 var food = "croissant is meh"
 console.log(food.replace('meh', 'so good'))
+// food = food.replace("meh","so good")
 
 // 06
 
-var basic = "This is Cool"
-var basicUp = "THIS IS COOL"
-var basicDown = "this is cool"
 
-console.log(basic,basicUp,basicDown)
+var basic = "This is Cool"
+var basicUp = basic.toUpperCase()
+var basicDown = basic.toLowerCase()
+console.log(basic)
+console.log(basicUp)
+console.log(basicDown)
 
 // 07
 
@@ -39,9 +40,9 @@ var letters= word.split("")
 
 console.log(letters)
 
+
 // 08
 
 var age = "18"
 var template = `I'm ${age} years old`
-
 console.log(template)
