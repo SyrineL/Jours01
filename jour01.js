fd264b3f23982b66e5f9618310e7025960eb5606
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-09-27 12:36:51 +0200

    first commit

---
+++ jour01.js
@@ -0,0 +1,47 @@
+// 01 - Hello World
+
+console.log("Hello World !");
+
+// 02
+
+var test = "My name is Syrine"
+
+console.log(test);
+
+// 03
+
+var name = " Syrine"
+
+console.log("Nice to meet you" + name)
+
+// 04
+
+var testLenght = "I'm very long !"
+console.log(`${testLenght.length}`)
+
+// 05
+
+var food = "croissant is meh"
+console.log(food.replace('meh', 'so good'))
+
+// 06
+
+var basic = "This is Cool"
+var basicUp = "THIS IS COOL"
+var basicDown = "this is cool"
+
+console.log(basic,basicUp,basicDown)
+
+// 07
+
+var word ="banana"
+var letters= word.split("")
+
+console.log(letters)
+
+// 08
+
+var age = "18"
+var template = `I'm ${age} years old`
+
+console.log(template)
